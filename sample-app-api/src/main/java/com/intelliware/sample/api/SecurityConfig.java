fa9c8da2608ead6ecbeb73894349d8aa8a8a2732
Diane Jansen <diane.jansen@intelliware.ca>
2015-04-27 13:42:39 -0400

    RUN-68 DJ/CP - login

--- sample-app-api/src/main/java/com/intelliware/sample/api/SecurityConfig.java
+++ sample-app-api/src/main/java/com/intelliware/sample/api/SecurityConfig.java
@@ -1,22 +1,36 @@
 package com.intelliware.sample.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic()
-				.and().authorizeRequests()
-					.antMatchers("/authentication", "/identities", "/").permitAll()
-					.anyRequest().authenticated()
+		http.httpBasic().authenticationEntryPoint(getAuthenticationEntryPoint())
+				.and().authorizeRequests().anyRequest().authenticated()
 				.and().logout()
 				.and().csrf().disable();
 	}
+	
+	@Bean
+	public AuthenticationEntryPoint getAuthenticationEntryPoint() {
+		return new RestAuthenticationEntryPoint();
+	}
 
+    @Autowired
+	@Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("a").password("b").roles("USER", "COMPANY");
+    }
 }
--- sample-app-api/src/main/java/com/intelliware/sample/api/controller/IdentityController.java
+++ sample-app-api/src/main/java/com/intelliware/sample/api/controller/IdentityController.java
@@ -1,17 +1,15 @@
 package com.intelliware.sample.api.controller;
 
-import java.security.Principal;
-
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IdentityController {
 
-	@RequestMapping(value="/authentication", method=RequestMethod.POST)//, headers={"content-type=application/json"})
-	public Principal user(Principal user) {
-		return user;
-	}
+	@RequestMapping(value="/me")
+    public Object user() {
+            return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
 
 }
