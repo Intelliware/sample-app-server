159aee387f7a402db7f62bc590619531fc0ad108
Diane Jansen <diane.jansen@intelliware.ca>
2015-04-28 16:23:58 -0400

    RUN-68 DJ - hardcoded user values for client

--- sample-app-api/src/main/java/com/intelliware/sample/api/controller/IdentityController.java
+++ sample-app-api/src/main/java/com/intelliware/sample/api/controller/IdentityController.java
@@ -1,15 +1,34 @@
 package com.intelliware.sample.api.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.intelliware.sample.vo.UserVO;
+
 @RestController
-public class IdentityController {
+public class IdentityController{
 
 	@RequestMapping(value="/me")
-    public Object user() {
-            return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
+	public UserVO user() {
+		UserDetails u = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		UserVO userVO = new UserVO();
+		userVO.setUsername(u.getUsername());
+		userVO.setId("0");
+//		Set<String> authoritySet = AuthorityUtils.authorityListToSet(u.getAuthorities());
+		Set<String> authoritySet = new HashSet<String>();
+		authoritySet.add("USER");
+		authoritySet.add("USER.EDIT");
+		authoritySet.add("USER.CREATE");
+		authoritySet.add("COMPANY");
+		authoritySet.add("COMPANY.EDIT");
+		authoritySet.add("COMPANY.CREATE");
+		userVO.setAuthorities(authoritySet);
+		return userVO;
+	}
 
 }
