2f874dda79f260d365d9cdb2e376e08bc0b27f0b
Diane Jansen <diane.jansen@intelliware.ca>
2015-04-28 15:52:00 -0400

    RUN-68 DJ/CP - added Flyway

--- sample-app-api/src/main/resources/data-h2.sql
+++
@@ -1,4 +0,0 @@
-insert into contact (email, first_name, last_name) values ('karyn.porter@stelaecor.com', 'Karyn', 'Porter');
-select @contact_id := scope_identity();
-insert into company (name, phone, contact_id) values ('MARVAME', '+1 (828) 533-2655', @contact_id);
-insert into company (name, phone, contact_id) values ('BOILICON', '+1 (893) 432-3827', @contact_id);
