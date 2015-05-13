0f2611351751954a6360c8418b5927a9b1efecea
Kevin Elhart <kevin.elhart@intelliware.com>
2015-05-13 10:04:30 -0400

    RUN-75: KE - if the getUser method receives an id of "-1", then just return an empty UserVO

---
+++ sample-app-api/src/main/java/com/intelliware/sample/api/dao/IDAOConstants.java
@@ -0,0 +1,6 @@
+package com.intelliware.sample.api.dao;
+
+public interface IDAOConstants {
+	public static final String NEW_ENTITY_ID_STRING = "-1"; // FIXME - KE - not sure about the name
+	public static final Long NEW_ENTITY_ID_LONG = -1L;
+}
