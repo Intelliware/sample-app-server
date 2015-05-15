6496f3f8a1b7f60185e616e0c05520e902928cba
JansenD <diane.jansen@intelliware.ca>
2015-05-15 12:20:16 -0400

    DJ technical debt: removed redundant modifiers

--- sample-app-server/src/main/java/com/intelliware/sample/api/controller/IConstants.java
+++ sample-app-server/src/main/java/com/intelliware/sample/api/controller/IConstants.java
@@ -2,7 +2,7 @@ package com.intelliware.sample.api.controller;
 
 public interface IConstants {
 
-	public static final String JSON_UTF8 = "application/json;charset=UTF-8";
-	public static final String MULTIPART_FORM_DATA = "multipart/form-data";
+	String JSON_UTF8 = "application/json;charset=UTF-8";
+	String MULTIPART_FORM_DATA = "multipart/form-data";
 	
 }
